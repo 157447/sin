8d3a31fc84e794b38bdf28896886b822707a7f18
student <student@Win64-8.ue.loc>
2017-03-17 14:22:41 +0100

    03/17

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/category', ['as' => 'category.get', 'uses' => 'CategoryController@get']);
+        
+
+    
---
+++ resources/views/categories.blade.php
@@ -0,0 +1,8 @@
+<ul>   
+@foreach ($categories as $category)
+<li>{{$category->name}}</li> 
+@endforeach
+</ul>
+
+
+
